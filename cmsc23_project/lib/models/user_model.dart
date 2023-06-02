74c6f01ae654bab887cd49ce79b4b56cbd3d5c97
caaal0 <jcsagun@up.edu.ph>
2023-06-02 16:06:07 +0800

    changed user model to adopt all user types

--- cmsc23_project/lib/models/user_model.dart
+++ cmsc23_project/lib/models/user_model.dart
@@ -1,29 +1,64 @@
 import 'dart:convert';
 
 class UserRecord {
-  // user properties
+  // base user properties
   String id;
-  String fname;
-  String lname;
+  String name;
   String email;
   List<String> entries;
+  bool isUnderMonitoring;
+  bool isQuarantined;
+  String userType;
+  
+  // SPECIFIC user data
+  String? username;
+  String? college;
+  String? course;
+  String? studno;
+  
+  //SPECIFC admin/entrance_monitor data
+  String? empno;
+  String? position;
+  String? unit;
 
   // constructor
   UserRecord(
       {required this.id,
-      required this.fname,
-      required this.lname,
+      required this.name,
       required this.email,
-      required this.entries});
+      required this.entries,
+      required this.isUnderMonitoring,
+      required this.isQuarantined,
+      required this.userType,
+
+      this.username,
+      this.college,
+      this.course,
+      this.studno,
+
+      this.empno,
+      this.position,
+      this.unit,
+      });
 
   // creates new instance of user record using data stored in json
   factory UserRecord.fromJson(Map<String, dynamic> json) {
     return UserRecord(
         id: json['id'],
-        fname: json['fname'],
-        lname: json['lname'],
+        name: json['name'],
         email: json['email'],
-        entries: json['entries']);
+        entries: json['entries'],
+        isUnderMonitoring: json['isUnderMonitoring'],
+        isQuarantined: json['isQuarantined'],
+        userType: json['userType'],
+        username: json['username'],
+        college: json['college'],
+        course: json['course'],
+        studno: json['studno'],
+        empno: json['empno'],
+        position: json['position'],
+        unit: json['unit'],
+      );
   }
 
   static List<UserRecord> fromJsonArray(String jsonData) {
@@ -33,10 +68,20 @@ class UserRecord {
 
   Map<String, dynamic> toJson(UserRecord user) {
     return {
-      'fname': user.fname,
-      'lname': user.lname,
+      'id': user.id,
+      'name': user.name,
       'email': user.email,
-      'entries': user.entries
+      'entries': user.entries,
+      'isUnderMonitoring': user.isUnderMonitoring,
+      'isQuarantined': user.isQuarantined,
+      'userType': user.userType,
+      'username': user.username,
+      'college': user.college,
+      'course': user.course,
+      'studno': user.studno,
+      'empno': user.empno,
+      'position': user.position,
+      'unit': user.unit,
     };
   }
 }
