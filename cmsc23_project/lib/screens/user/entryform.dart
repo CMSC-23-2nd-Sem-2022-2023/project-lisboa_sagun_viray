cb2fe34196d97f17b628090e12a0703815b40759
inno29 <ivlisboa@up.edu.ph>
2023-06-03 13:28:43 +0000

    fixed refresh issue

--- cmsc23_project/lib/screens/user/entryform.dart
+++ cmsc23_project/lib/screens/user/entryform.dart
@@ -1,13 +1,15 @@
 import 'dart:core';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../models/entry_model.dart';
-import '../../providers/entry_provider.dart';
+// import '../../api/firebase_entry_api.dart';
+// import '../../api/firebase_auth_api.dart';
 import '../../providers/auth_provider.dart';
+import '../../providers/entry_provider.dart';
 
 class EntryForm extends StatefulWidget {
   const EntryForm({super.key});
@@ -45,11 +47,14 @@ class _EntryFormState extends State<EntryForm> {
   Widget formHeader() {
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('yyyy-MM-dd').format(now);
+    setState(() {
+      dateController.text = formattedDate;
+    });
     return Column(children: [
-      SizedBox(
+      const SizedBox(
         height: 20,
       ),
-      Text(
+      const Text(
         'ENTRY FORM',
         style: TextStyle(
             fontSize: 50,
@@ -58,12 +63,12 @@ class _EntryFormState extends State<EntryForm> {
       ),
       Text(
         'Date today: $formattedDate',
-        style: TextStyle(
+        style: const TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.bold,
             color: Color.fromARGB(255, 0, 37, 67)),
       ),
-      SizedBox(
+      const SizedBox(
         height: 20,
       )
     ]);
@@ -72,16 +77,38 @@ class _EntryFormState extends State<EntryForm> {
   Widget symptomsCheckbox() {
     return Column(
       children: [
-        Divider(
+        const Divider(
           thickness: 5,
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
-        Text(
+        const Text(
           "Do you have any pre-exisiting illness? Check all that applies.",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
         ),
+        // TextField(
+        //     controller: dateController, //editing controller of this TextField
+        //     decoration: const InputDecoration(
+        //         icon: Icon(Icons.calendar_today), //icon of text field
+        //         labelText: "Enter Date"),
+        //     readOnly: true,
+        //     onTap: () async {
+        //       DateTime? pickedDate = await showDatePicker(
+        //           context: context,
+        //           initialDate: DateTime.now(), //get today's date
+        //           firstDate: DateTime.now(),
+        //           lastDate: DateTime(2101));
+
+        //       if (pickedDate != null) {
+        //         String formattedDate =
+        //             DateFormat('yyyy-MM-dd').format(pickedDate);
+
+        //         setState(() {
+        //           dateController.text = formattedDate;
+        //         });
+        //       } else {}
+        //     }),
         ListView.builder(
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
@@ -90,7 +117,7 @@ class _EntryFormState extends State<EntryForm> {
             itemExtent: 40,
             itemBuilder: (BuildContext context, int index) {
               return CheckboxListTile(
-                activeColor: Color.fromARGB(255, 0, 37, 67),
+                activeColor: const Color.fromARGB(255, 0, 37, 67),
                 controlAffinity: ListTileControlAffinity.leading,
                 title: Text(
                   symptoms[index],
@@ -117,13 +144,13 @@ class _EntryFormState extends State<EntryForm> {
   Widget covidContact() {
     return Column(
       children: [
-        Divider(
+        const Divider(
           thickness: 5,
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
-        Text('Have you come in contact with a confirmed COVID-19 case?',
+        const Text('Have you come in contact with a confirmed COVID-19 case?',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
         ListView.builder(
             scrollDirection: Axis.vertical,
@@ -157,125 +184,115 @@ class _EntryFormState extends State<EntryForm> {
     );
   }
 
-  Widget submitAndResetButtons() {
-    return Column(
-      children: [
-        Divider(
-          thickness: 5,
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        ElevatedButton(
-          style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all<Size>(
-              Size(double.infinity, 50), // Adjust the width here
-            ),
-            backgroundColor: MaterialStateProperty.all<Color>(
-                Color.fromARGB(255, 0, 67, 27)),
-            foregroundColor: MaterialStateProperty.all<Color>(
-                Color.fromARGB(255, 0, 67, 27)),
-            shape: MaterialStateProperty.all<StadiumBorder>(
-              StadiumBorder(),
-            ),
-          ),
-          onPressed: () async {
-            // if (_formKey.currentState!.validate()) {
-            //   await context.read<AuthProvider>().signIn(
-            //         emailController.text.trim(),
-            //         passwordController.text.trim(),
-            //       );
-            // }
-          },
-          child: Text('SUBMIT ENTRY', style: TextStyle(color: Colors.white)),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        ElevatedButton(
-            style: ButtonStyle(
-              minimumSize: MaterialStateProperty.all<Size>(
-                Size(double.infinity, 50), // Adjust the width here
-              ),
-              backgroundColor: MaterialStateProperty.all<Color>(
-                  Color.fromARGB(255, 67, 0, 0)),
-              foregroundColor: MaterialStateProperty.all<Color>(
-                  Color.fromARGB(255, 67, 0, 0)),
-              shape: MaterialStateProperty.all<StadiumBorder>(
-                StadiumBorder(),
-              ),
-            ),
-            onPressed: () {
-              setState(() {
-                for (var i = 0; i < isCheckedList.length; i++) {
-                  isCheckedList[i] = false;
-                }
-                isInContact = choices[0];
-                Navigator.pop(context);
-              });
-            },
-            child: Text('RESET', style: TextStyle(color: Colors.white))),
-      ],
-    );
+  Widget submitAndResetButtons(BuildContext context, Stream<User?> userStream) {
+    return StreamBuilder(
+        stream: userStream,
+        builder: (context, AsyncSnapshot<User?> snapshot) {
+          print(snapshot.connectionState);
+          print(snapshot.data);
+          if (snapshot.connectionState == ConnectionState.active) {
+            User? user = snapshot.data;
+            String? userId = user?.uid;
+            return Column(
+              children: [
+                const Divider(
+                  thickness: 5,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all<Size>(
+                      const Size(double.infinity, 50), // Adjust the width here
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 0, 67, 27)),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromARGB(255, 0, 67, 27)),
+                    shape: MaterialStateProperty.all<StadiumBorder>(
+                      const StadiumBorder(),
+                    ),
+                  ),
+                  onPressed: () async {
+                    print('button pressed');
+                    print(dateController.text);
+                    Entry entry = Entry(
+                        UID: userId!,
+                        date: dateController.text,
+                        symptoms: isCheckedList,
+                        hasContact: contactCheck);
+                    context.read<EntryListProvider>().addEntry(entry);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('SUBMIT ENTRY',
+                      style: TextStyle(color: Colors.white)),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all<Size>(
+                        const Size(
+                            double.infinity, 50), // Adjust the width here
+                      ),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(255, 67, 0, 0)),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(255, 67, 0, 0)),
+                      shape: MaterialStateProperty.all<StadiumBorder>(
+                        const StadiumBorder(),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        for (var i = 0; i < isCheckedList.length; i++) {
+                          isCheckedList[i] = false;
+                        }
+                        isInContact = choices[0];
+                      });
+                    },
+                    child: const Text('RESET',
+                        style: TextStyle(color: Colors.white))),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    Stream<User?> userStream = context.read<AuthProvider>().uStream;
-
-    return StreamBuilder(
-      stream: userStream,
-      builder: (context, snapshot) {
-        print(snapshot.connectionState);
-        if (snapshot.hasError) {
-          return Center(
-            child: Text("Error encountered! ${snapshot.error}"),
-          );
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (!snapshot.hasData) {
-          return const Center(
-            child: Text("snapshot has no data"),
-          );
-        }
-
-        String UID = snapshot.data!.uid;
-
-        // if user is logged in, display the scaffold containing the streambuilder for the todos
-        return displayScaffold(context, UID);
-      },
-    );
-  }
-
-  Scaffold displayScaffold(BuildContext context, String UID) {
-    print('$UID is logged in');
+    context.read<AuthProvider>().fetchAuthentication();
+    Stream<User?> userStream = context.watch<AuthProvider>().uStream;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 0, 37, 67),
-      ),
-      //TODO make a form that accepts entries
-      body: ListView(
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(20),
-        children: [
-          formHeader(),
-          Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                symptomsCheckbox(),
-                SizedBox(
-                  height: 15,
-                ),
-                covidContact(),
-                submitAndResetButtons(),
-              ],
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 0, 37, 67),
+        ),
+        //TODO make a form that accepts entries
+        body: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20),
+          children: [
+            formHeader(),
+            Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  symptomsCheckbox(),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  covidContact(),
+                  submitAndResetButtons(context, userStream),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
