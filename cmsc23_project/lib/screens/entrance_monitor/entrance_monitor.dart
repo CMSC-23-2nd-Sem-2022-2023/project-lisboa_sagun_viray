10514753f2fcbef17612faf1f60e9a139c7dc5ac
unknown <ivlisboa@up.edu.ph>
2023-06-08 00:40:37 +0800

    added entrance monitor functionality

--- cmsc23_project/lib/screens/entrance_monitor/entrance_monitor.dart
+++ cmsc23_project/lib/screens/entrance_monitor/entrance_monitor.dart
@@ -12,6 +12,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:cmsc23_project/screens/user/entryform.dart';
+import 'package:cmsc23_project/screens/user/editform.dart';
 import 'dart:convert';
 
 class EntranceMonitor extends StatefulWidget {
@@ -61,6 +63,57 @@ class _EntranceMonitorState extends State<EntranceMonitor> {
     );
   }
 
+  Widget hasContactWidget(entry) {
+    for (int i = 0; i < entry.symptoms.length; i++) {
+      if (entry.symptoms[i] == true) {
+        // print("hey");
+        return Row(
+          children: [
+            Center(
+              child: Text(
+                "Has Symptoms: ",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: Text("TRUE"),
+            )
+          ],
+        );
+        break;
+      } else {
+        // print("hello");
+        return Row(
+          children: [
+            Center(
+              child: Text(
+                "has Symptoms: ",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Center(
+              child: Text("FALSE"),
+            )
+          ],
+        );
+      }
+    }
+    return Text("back to u hey");
+    return Row(
+      children: [
+        Center(
+          child: Text(
+            "has Symptoms: ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Center(
+          child: Text("FALSE"),
+        )
+      ],
+    );
+  }
+
   Widget searchBar() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -94,54 +147,212 @@ class _EntranceMonitorState extends State<EntranceMonitor> {
               child: Text("Error encountered! ${snapshot.error}"),
             );
           } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+            return const Center(child: Text('this is causing an error'));
           } else if (!snapshot.hasData) {
             return const Center(
               child: Text("No Entries Found"),
             );
           }
 
-          return ListView.builder(
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListView.builder(
               itemCount: snapshot.data?.docs.length,
               itemBuilder: (context, index) {
                 Entry entry = Entry.fromJson(
-                    snapshot.data?.docs[index].data() as Map<String, dynamic>);
+                  snapshot.data?.docs[index].data() as Map<String, dynamic>,
+                );
                 entries.add(entry);
-                //access entry like 'entry.'
-                return ListTile(
-                  title: Text(
-                    entry.date + " ENTRY",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  // leading: Text(entry.date),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          // Perform edit operation for the entry
-                          // You can navigate to an edit screen or show a dialog
-                          // to allow the user to modify the entry.
-                          // Example: navigate to EditEntryScreen(entry);
+
+                return SizedBox(
+                  height: 80,
+                  child: Card(
+                    elevation: 9,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30))),
+                    child: Center(
+                      child: ListTile(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  title: Center(
+                                      child: Text(
+                                    'ENTRY DETAILS',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )),
+                                  content: Container(
+                                    width: 200,
+                                    height: 80,
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Center(
+                                              child: Text(
+                                                "Date Submitted: ",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Center(
+                                              child: Text(entry.date),
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Center(
+                                              child: Text(
+                                                "Has Contact: ",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Center(
+                                              child: Text(entry.hasContact
+                                                  .toString()
+                                                  .toUpperCase()),
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Center(
+                                              child: Text(
+                                                "Status: ",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            Center(
+                                              child:
+                                                  Text(entry.status.toString()),
+                                            )
+                                          ],
+                                        ),
+                                        hasContactWidget(entry),
+                                      ],
+                                    ),
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              Color.fromARGB(255, 0, 37, 67),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          ),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text("CLOSE")),
+                                  ],
+                                );
+                              });
                         },
+                        title: Text(
+                          entry.date + " ENTRY",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        leading: CircleAvatar(
+                          backgroundColor: Color.fromARGB(255, 0, 37, 67),
+                          child: Text((index + 1).toString()),
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                Icons.edit,
+                                color: Color.fromARGB(255, 0, 37, 67),
+                              ),
+                              onPressed: () {
+                                String? docrefID = entry.id;
+                                String? status = entry.status;
+                                if (status == "open") {
+                                  context
+                                      .read<EntryListProvider>()
+                                      .entryPendingEdit(docrefID);
+                                  context
+                                      .read<EntryListProvider>()
+                                      .setToEdit(docrefID);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content:
+                                          Text("Entry is now pending for edit"),
+                                    ),
+                                  );
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => EditForm()));
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Entry is already pending"),
+                                    ),
+                                  );
+                                }
+
+                                // Perform edit operation for the entry
+                                // You can navigate to an edit screen or show a dialog
+                                // to allow the user to modify the entry.
+                                // Example: navigate to EditEntryScreen(entry);
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                Icons.delete,
+                                color: Color.fromARGB(255, 0, 37, 67),
+                              ),
+                              onPressed: () {
+                                String? docrefID = entry.id;
+                                String? status = entry.status;
+                                if (status == "open") {
+                                  context
+                                      .read<EntryListProvider>()
+                                      .entryPendingDelete(docrefID);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                          "Entry is now pending for delete"),
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Entry is already pending"),
+                                    ),
+                                  );
+                                }
+
+                                // Perform edit operation for the entry
+                                // You can navigate to an edit screen or show a dialog
+                                // to allow the user to modify the entry.
+                                // Example: navigate to EditEntryScreen(entry);
+                              },
+                            ),
+                          ],
+                        ),
                       ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          // Perform delete operation for the entry
-                          // You can show a confirmation dialog before deleting
-                          // the entry to confirm the user's intent.
-                          // Example: showDeleteConfirmationDialog(entry);
-                        },
-                      ),
-                    ],
+                    ),
                   ),
                 );
-              });
-          // return Center();
+              },
+            ),
+          );
+// return Center();
         });
   }
 
